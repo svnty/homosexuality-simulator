21b7f4ac4379aa53e93de073030a02af82e242e6
Jake Walklate <jakewalklate@gmail.com>
2022-02-14 14:52:26 +1100

    typescript

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { appendFileSync } from 'fs';
+import { Person } from './assets/Person';
+
+let generation: number = 0;
+let homosexual_mates: number = 0;
+let humans: [] = [];
+let donors: [] = [];
+let children_range = 0;
+let child_counter = 0;
+let children_percent = 0;
+let homosexual_counter = 0;
+let homosexual_event_count = 0;
+let total_child_count = 0;
+
