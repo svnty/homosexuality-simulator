96ee6a61a9c401c14c3ac04b316e8242ab7c7286
Jake Walklate <jakewalklate@gmail.com>
2022-05-26 15:53:10 +1000

    update

--- assets/configuration.ts
+++ assets/configuration.ts
@@ -1,12 +1,15 @@
 export default {
     "starting_population": 1_000,
-    "init_random_sexuality": true,
+    "init_all_heterosexual": false,
     "starting_lgbt_percent": 0,
     "offspring_percent": [0.0175, 0.013],
     "random_event_chance": 0,
+    "force_homosexual_random_event_during_generation": false,
     "death_range": [90, 70],
     "breed_range": [20, 50],
     "sociological_toggle": false,
+    "homosexual_only_chance": false,
+    "homosexual_only_chance_percent": 0.1,
     "sociological_chance": 0,
     "run_off": 500
 }
